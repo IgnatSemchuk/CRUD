50ab25730ead1f8b784ddbf4ff180400ec648549
IgnatSemchuk <semchukignat@gmail.com>
2019-02-06 03:19:50 +0200

    added pagination

--- app/js/UsersTable.js
+++ app/js/UsersTable.js
@@ -5,20 +5,66 @@ export class UsersTable {
   constructor() {
     this.users = [];
     this.backend = new AppBackend('https://5bf417c491c25b0013a3b9a2.mockapi.io/users');
+    this.page = this.initPage();
+    this.limitRows;
   }
 
   async init() {
     this.render();
+    this.limitRows = document.getElementById('si-input-limit').value;
     await this.loadUsers();
     this.renderUsersTable();
 
+    const next = document.querySelector('[data-next]');
+    const prev = document.querySelector('[data-preview]');
+    
     document.addEventListener('click', async (event) => {
+      if (event.target.matches('[data-preview]')) {
+        if (next.hasAttribute('disabled')) {
+          next.removeAttribute('disabled');
+        };
+
+        if (this.page() === 2) {
+          event.target.setAttribute('disabled', 'disabled');
+        }
+        this.page(-1);
+        await this.loadUsers();
+        this.renderUsersTable();
+      }
+
+
+      if (event.target.matches('[data-next]')) {
+        if (prev.hasAttribute('disabled')) {
+          prev.removeAttribute('disabled');
+        };
+
+        this.page(1);
+        await this.loadUsers();
+        if (this.users.length < this.limitRows - 1) {
+          event.target.setAttribute('disabled', 'disabled');
+        }
+        if (this.users.length === 0) {
+          event.target.setAttribute('disabled', 'disabled');
+          this.page(-1);
+          await this.loadUsers();
+        }
+        this.renderUsersTable();
+      }
+
+
       if (event.target.matches('[data-delete]')) {
         await this.backend.delete(event.target.dataset.id);
         await this.loadUsers();
         this.renderUsersTable();
       };
-    }, false);
+
+      if (event.target.matches('[data-edit]')) {
+        const targetRow = event.target.closest('tr');
+        console.dir(targetRow, this.page);
+        await this.loadUsers();
+        this.renderUsersTable();
+      };
+    });
 
     document.getElementById('si-add-user-form').addEventListener('submit', async (event) => {
       event.preventDefault();
@@ -32,26 +78,30 @@ export class UsersTable {
       await this.backend.create(options);
       await this.loadUsers();
       this.renderUsersTable();
-    }, false);
+    });
 
     document.getElementById('si-input-limit').addEventListener('change', async (event) => {
+      this.limitRows = event.target.value;
+      this.page = this.initPage();
       await this.loadUsers();
       this.renderUsersTable();
-    }, false);
+    });
+  }
 
-
-
+  initPage() {
+      let page = 1;
+      return (step = 0) => {
+        page += step;
+        return page;
+      }
   }
 
   async loadUsers() {
-    const limit = document.getElementById('si-input-limit').value;
-    const options = {
-      page: 1,
-      limit,
-    };
-    console.log(typeof options);
+    const options = {};
+    options.page = this.page();
+    options.limit = this.limitRows;
+    console.log(options);
     this.users = await this.backend.get(options);
-    console.log(this.users);
   }
 
   renderUsersTable() {
@@ -165,10 +215,10 @@ export class UsersTable {
 
         <div class="row si-footer-table">
           <div class="col-auto ml-auto">
-            <button type="button" class="btn btn-primary"data-target="prev">
+            <button type="button" class="btn btn-primary" data-preview disabled>
               Preview
             </button>
-            <button type="button" class="btn btn-primary"data-target="next">
+            <button type="button" class="btn btn-primary" data-next>
               Next
             </button>
           </div>
